e33b61d25e6cbcb3e8e7e8a23ed350ede81ccaed
axel.scheithauer@gmail.com <axel.scheithauer@gmail.com@04f10c37-a2c5-ef92-8775-48cd31e289db>
2010-10-24 17:57:38 +0000

    das fokussierte Feld wird jetzt mit 1 vorbelegt, das Andere entsprechend berechnet.

--- trunk/Kiwi/src/de/oose/wechselkurs/main.java
+++ trunk/Kiwi/src/de/oose/wechselkurs/main.java
@@ -5,6 +5,8 @@ import java.text.NumberFormat;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnFocusChangeListener;
 import android.widget.TextView;
 
 public class main extends Activity {
@@ -14,6 +16,16 @@ public class main extends Activity {
 	public static final String Currency2 = "NZD";
 	public static final double NORMALIZED_RATE = 1;
 
+	private OnFocusChangeListener value1Listener= new OnFocusChangeListener(){
+		public void onFocusChange(View v, boolean hasFocus) {
+			if (hasFocus){setValue1(1);}
+		}
+	};	
+	private OnFocusChangeListener value2Listener= new OnFocusChangeListener(){
+		public void onFocusChange(View v, boolean hasFocus) {
+			if (hasFocus){setValue2(1);}
+		}
+	};	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -26,7 +38,9 @@ public class main extends Activity {
 		value1.setText(formatCurrency(NORMALIZED_RATE,Currency1));
 		value2.setText(formatCurrency(rate,Currency2));
 		value1.requestFocus();
-	}
+		value1.setOnFocusChangeListener(value1Listener);
+		value2.setOnFocusChangeListener(value2Listener);
+		}
 	
 	String formatCurrency(double aValue, String currency){
 		NumberFormat nf = NumberFormat.getInstance();
@@ -37,5 +51,15 @@ public class main extends Activity {
 			// ignore		
 			}
 		return nf.format(aValue) + " " + currency;
+	}
+
+	private void setValue1(double aValue) {
+		value1.setText(formatCurrency(aValue,Currency1));
+		value2.setText(formatCurrency(rate*aValue,Currency2));
+	}
+
+	private void setValue2(double aValue) {
+		value1.setText(formatCurrency(aValue/rate,Currency1));
+		value2.setText(formatCurrency(aValue,Currency2));	
 	}
 }
