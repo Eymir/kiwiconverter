f27212740a931e7347a084733c921cc1a8c3efd3
axel.scheithauer <axel.scheithauer@gmail.com>
2010-10-24 16:55:20 +0000

    Layout aufgehübscht mit InputText-Boxen, Rate wird in Attribut gespeichert.

--- Kiwi/src/de/oose/wechselkurs/main.java
+++ Kiwi/src/de/oose/wechselkurs/main.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class main extends Activity {
-	public static final String EURO = "EUR";
-	public static final String NZD = "NZD";
+	private TextView value1, value2;
+	private double rate;
+	public static final String Currency1 = "EUR";
+	public static final String Currency2 = "NZD";
 	public static final String NORMALIZED_RATE = "1";
 
 	/** Called when the activity is first created. */
@@ -14,20 +16,12 @@ public class main extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		String text = currencyStringBuilder(EURO, NZD);
-		TextView view = (TextView) findViewById(R.id.conversion1);
-		view.setText(text);
-
-		text = currencyStringBuilder(NZD, EURO);
-		view = (TextView) findViewById(R.id.conversion2);
-		view.setText(text);
-
-	}
-
-	private String currencyStringBuilder(String from, String to) {
+		value1 = (TextView) findViewById(R.id.value1);
+		value2 = (TextView) findViewById(R.id.value2);
 		CurrencyService currencyService = new YahooFinance();
-		String text = NORMALIZED_RATE + from + " " + getText(R.string.ist)
-				+ " " + currencyService.getRate(from, to) + to;
-		return text;
+		rate=currencyService.getRate(Currency1, Currency2);
+		value1.setText(NORMALIZED_RATE + " "+ Currency1 );
+		value2.setText(rate + " " + Currency2);
+		value1.requestFocus();
 	}
 }
