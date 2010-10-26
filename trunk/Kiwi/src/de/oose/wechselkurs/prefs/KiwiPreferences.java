8d74c6a52360783992fd60e19b002f656a3e2972
StefanZoerner@gmail.com <StefanZoerner@gmail.com@04f10c37-a2c5-ef92-8775-48cd31e289db>
2010-10-26 17:08:52 +0000

    Activity fuer Einstellungen

---
+++ trunk/Kiwi/src/de/oose/wechselkurs/prefs/KiwiPreferences.java
@@ -0,0 +1,28 @@
+package de.oose.wechselkurs.prefs;
+
+import android.content.ContextWrapper;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import de.oose.wechselkurs.R;
+
+/**
+ * Activity zur Einstellung der Kiwi-Applikation.
+ * 
+ * @author StefanZ
+ */
+public class KiwiPreferences extends PreferenceActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		this.addPreferencesFromResource(R.xml.kiwi_preferences);
+	}
+
+	public static SharedPreferences getPreferences(ContextWrapper ctx) {
+		return ctx.getSharedPreferences(ctx.getPackageName() + "_preferences",
+				MODE_PRIVATE);
+	}
+
+}
