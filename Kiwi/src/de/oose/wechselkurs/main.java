9cb7f47cb70435439e8cdaade74be47ae9e4bab1
StefanZoerner <StefanZoerner@gmail.com>
2010-10-26 17:28:22 +0000

    Exception-Handler spendiert (mit Logging)

--- Kiwi/src/de/oose/wechselkurs/main.java
+++ Kiwi/src/de/oose/wechselkurs/main.java
@@ -5,6 +5,7 @@ import java.text.NumberFormat;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnFocusChangeListener;
 import android.widget.TextView;
@@ -16,50 +17,59 @@ public class main extends Activity {
 	public static final String Currency2 = "NZD";
 	public static final double NORMALIZED_RATE = 1;
 
-	private OnFocusChangeListener value1Listener= new OnFocusChangeListener(){
+	private OnFocusChangeListener value1Listener = new OnFocusChangeListener() {
 		public void onFocusChange(View v, boolean hasFocus) {
-			if (hasFocus){setValue1(1);}
+			if (hasFocus) {
+				setValue1(1);
+			}
 		}
-	};	
-	private OnFocusChangeListener value2Listener= new OnFocusChangeListener(){
+	};
+	private OnFocusChangeListener value2Listener = new OnFocusChangeListener() {
 		public void onFocusChange(View v, boolean hasFocus) {
-			if (hasFocus){setValue2(1);}
+			if (hasFocus) {
+				setValue2(1);
+			}
 		}
-	};	
+	};
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		value1 = (TextView) findViewById(R.id.value1);
-		value2 = (TextView) findViewById(R.id.value2);
-		CurrencyService currencyService = new YahooFinance();
-		rate=currencyService.getRate(Currency1, Currency2);
-		value1.setText(formatCurrency(NORMALIZED_RATE,Currency1));
-		value2.setText(formatCurrency(rate,Currency2));
-		value1.requestFocus();
-		value1.setOnFocusChangeListener(value1Listener);
-		value2.setOnFocusChangeListener(value2Listener);
+		try {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.main);
+			value1 = (TextView) findViewById(R.id.value1);
+			value2 = (TextView) findViewById(R.id.value2);
+			CurrencyService currencyService = new YahooFinance();
+			rate = currencyService.getRate(Currency1, Currency2);
+			value1.setText(formatCurrency(NORMALIZED_RATE, Currency1));
+			value2.setText(formatCurrency(rate, Currency2));
+			value1.requestFocus();
+			value1.setOnFocusChangeListener(value1Listener);
+			value2.setOnFocusChangeListener(value2Listener);
+		} catch (Exception e) {
+			Log.e("KiwiApp", e.getMessage(), e);
 		}
-	
-	String formatCurrency(double aValue, String currency){
+	}
+
+	String formatCurrency(double aValue, String currency) {
 		NumberFormat nf = NumberFormat.getInstance();
 		try {
-			((DecimalFormat)nf).setMaximumFractionDigits(2);
-			((DecimalFormat)nf).setMinimumFractionDigits(2);
+			((DecimalFormat) nf).setMaximumFractionDigits(2);
+			((DecimalFormat) nf).setMinimumFractionDigits(2);
 		} catch (Exception e) {
-			// ignore		
-			}
+			// ignore
+		}
 		return nf.format(aValue) + " " + currency;
 	}
 
 	private void setValue1(double aValue) {
-		value1.setText(formatCurrency(aValue,Currency1));
-		value2.setText(formatCurrency(rate*aValue,Currency2));
+		value1.setText(formatCurrency(aValue, Currency1));
+		value2.setText(formatCurrency(rate * aValue, Currency2));
 	}
 
 	private void setValue2(double aValue) {
-		value1.setText(formatCurrency(aValue/rate,Currency1));
-		value2.setText(formatCurrency(aValue,Currency2));	
+		value1.setText(formatCurrency(aValue / rate, Currency1));
+		value2.setText(formatCurrency(aValue, Currency2));
 	}
 }
