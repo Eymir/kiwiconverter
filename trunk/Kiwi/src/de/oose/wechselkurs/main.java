88c99647e3d1746abc628f4618cad090a2deb5a3
axel.scheithauer@gmail.com <axel.scheithauer@gmail.com@04f10c37-a2c5-ef92-8775-48cd31e289db>
2010-10-28 22:08:09 +0000

    Preferences werden gelesen. Wert kann über logarithmische Skala eingestellt werden. Bekannte Fehler:(siehe TODO) geänderte Währungen werden erst nach Neustart verwendet. Die Anzeige beginnt nicht bei 1 sondern bei 1024.

--- trunk/Kiwi/src/de/oose/wechselkurs/main.java
+++ trunk/Kiwi/src/de/oose/wechselkurs/main.java
@@ -5,21 +5,27 @@ import java.text.NumberFormat;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnFocusChangeListener;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import de.oose.wechselkurs.prefs.KiwiPreferences;
 
 public class main extends Activity {
 	private TextView value1, value2;
 	private double rate;
-	public static final String Currency1 = "EUR";
-	public static final String Currency2 = "NZD";
+	private String currency1 = "EUR";
+	private String currency2 = "NZD";
+	private ScrollView sliderule; 
 	public static final double NORMALIZED_RATE = 1;
+	private static final int ACTIVITY_PREFS=1;
 
 	private OnFocusChangeListener value1Listener = new OnFocusChangeListener() {
 		public void onFocusChange(View v, boolean hasFocus) {
@@ -44,17 +50,30 @@ public class main extends Activity {
 			setContentView(R.layout.main);
 			value1 = (TextView) findViewById(R.id.value1);
 			value2 = (TextView) findViewById(R.id.value2);
-			CurrencyService currencyService = new YahooFinance();
-			rate = currencyService.getRate(Currency1, Currency2);
-			value1.setText(formatCurrency(NORMALIZED_RATE, Currency1));
-			value2.setText(formatCurrency(rate, Currency2));
+			sliderule = (ScrollView) findViewById(R.id.sliderule);
+			//final LinearLayout scale = (LinearLayout) findViewById(R.id.scale);
 			value1.requestFocus();
 			value1.setOnFocusChangeListener(value1Listener);
 			value2.setOnFocusChangeListener(value2Listener);
-		} catch (Exception e) {
+			
+			initCurrencies();
+
+			sliderule.scrollTo(0, 799); //TODO: warum geht das nicht??
+			sliderule.setOnTouchListener(new View.OnTouchListener() {
+				
+				public boolean onTouch(View v, MotionEvent event) {
+					double value =Math.pow(2,((double)(800 - sliderule.getScrollY()))/80);
+					if (value1.hasFocus()){
+						setValue1(value);} 
+					else{
+						setValue2(value);};
+					return false;
+				};
+			});
+			} catch (Exception e) {
 			Log.e("KiwiApp", e.getMessage(), e);
 		}
-	}
+	};
 
 	String formatCurrency(double aValue, String currency) {
 		NumberFormat nf = NumberFormat.getInstance();
@@ -68,13 +87,13 @@ public class main extends Activity {
 	}
 
 	private void setValue1(double aValue) {
-		value1.setText(formatCurrency(aValue, Currency1));
-		value2.setText(formatCurrency(rate * aValue, Currency2));
+		value1.setText(formatCurrency(aValue, currency1));
+		value2.setText(formatCurrency(rate * aValue, currency2));
 	}
 
 	private void setValue2(double aValue) {
-		value1.setText(formatCurrency(aValue / rate, Currency1));
-		value2.setText(formatCurrency(aValue, Currency2));
+		value1.setText(formatCurrency(aValue / rate, currency1));
+		value2.setText(formatCurrency(aValue, currency2));
 	}
 
 	@Override
@@ -88,7 +107,7 @@ public class main extends Activity {
 		switch (item.getItemId()) {
 		case R.id.main_menu_prefs:
 			Intent intent = new Intent(this, KiwiPreferences.class);
-			startActivity(intent);
+			startActivityForResult(intent,ACTIVITY_PREFS);
 			return true;
 		case R.id.main_menu_close:
 			this.finish();
@@ -96,5 +115,33 @@ public class main extends Activity {
 		default:
 			return super.onOptionsItemSelected(item);
 		}
+	}
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		Log.d("ActivityResult" ,"");//TODO: Wird nie aufgerufen. Warum?
+		if (requestCode==ACTIVITY_PREFS && resultCode != RESULT_CANCELED) {
+			initCurrencies();
+		}
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+
+
+	
+	private void initCurrencies() {
+		SharedPreferences prefs = KiwiPreferences.getPreferences(getApplication());
+		currency1 = prefs.getString("prefs_currency_1", currency1);
+		currency2 = prefs.getString("prefs_currency_2", currency2);
+		updateRate();
+	}
+
+	private void updateRate() {
+		CurrencyService currencyService = new YahooFinance();
+		rate=currencyService.getRate(currency1, currency2);
+		double value =Math.pow(2,((double)(800 - sliderule.getScrollY()))/80);
+		if (value1.hasFocus()){
+			setValue1(value);} 
+		else{
+			setValue2(value);};
+
 	}
 }
